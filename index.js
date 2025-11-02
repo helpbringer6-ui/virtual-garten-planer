2c2df6c087f77cb41807e6be82ed565bc2022d7a
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-11-02 04:13:05 -0600

    Refactor greeting message for clarity and brevity.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Willkommen zu Ihrem Virtuellen Garten Planer! Lassen Sie uns gemeinsam Ihren Traumgarten gestalten.');
+  res.send('Willkommen im Gartenplaner! Lassen Sie uns Ihren Traumgarten gestalten.');
 });
 
 const PORT = process.env.PORT || 3000;
